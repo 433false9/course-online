9fece2008c1f5b6512d1c1403e121be5eb14444f
ye <2012837965@qq.com>
2021-03-12 10:27:04 +0800

    Initial commit

---
+++ src/main/java/com/ye/courseonline/CourseOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.ye.courseonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseOnlineApplication.class, args);
+    }
+
+}
